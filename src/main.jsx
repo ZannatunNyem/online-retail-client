812294569b4dba346740cd952469b2e83c74b046
Zannatun Nyem Naziat <z.naziat@gmail.com>
2026-02-19 10:13:44 +0600

    one

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router.jsx";
+import AuthProvider from "./Auth/AuthProvider.jsx";
+import CartProvider from "./Auth/CartProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
